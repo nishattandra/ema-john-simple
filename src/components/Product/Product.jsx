4c53bc8a6ed0a504a927149119a73f12ecc15e34
nishattandra <nishattandra2001@gmail.com>
2023-03-27 16:24:29 +0600

    added Add to Cart Button

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,6 +3,8 @@ import './Product.css'
 
 const Product = (props) => {
     const { img, name, seller, ratings, price } = props.product;
+
+    const handleAddToCart = props.handleAddToCart
     return (
         <div className='product'>
             <img src={img} alt="" />
@@ -12,7 +14,7 @@ const Product = (props) => {
                 <p>Manufacturer: {seller}</p>
                 <p>Ratings: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={() =>handleAddToCart(props.product)} className='btn-cart'>Add to Cart</button>
         </div>
     );
 };
